fb7071c926a97e8b4ede429d3f771b158a062558
Aaron <ravycrow@gmail.com>
2024-03-06 21:44:41 +0000

    BackDone3.0

--- database/index.js
+++ database/index.js
@@ -3,6 +3,7 @@ const { Sequelize } = require('sequelize');
 const connection = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
  host: process.env.DB_HOST,
 dialect: process.env.DB_DIALECT, 
+port: process.env.DB_PORT,
 logging: false
 });
 
